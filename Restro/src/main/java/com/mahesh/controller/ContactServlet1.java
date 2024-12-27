2b45fe6985b92ddfece869d1ac7fc73bc014ae4b
mahesh8497 <maheshmtakale97@gmail.com>
2024-12-27 22:19:25 +0530

    add

--- Restro/src/main/java/com/mahesh/controller/ContactServlet1.java
+++ Restro/src/main/java/com/mahesh/controller/ContactServlet1.java
@@ -1,11 +1,14 @@
 package com.mahesh.controller;
 
 import java.io.IOException;
+import java.net.Authenticator.RequestorType;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.mahesh.service.ContactServiceImpl;
 
@@ -17,8 +20,11 @@ import com.mahesh.service.ContactServiceImpl;
 public class ContactServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session=request.getSession();
+		
 		// TODO Auto-generated method stub
 		String name=request.getParameter("name");
 		String email=request.getParameter("email");
@@ -29,8 +35,16 @@ public class ContactServlet1 extends HttpServlet {
 		ContactServiceImpl contactServiceImpl=new ContactServiceImpl();
 		String result=contactServiceImpl.ContactService1(name, email, subject, message);
 		
-		System.out.println(result);
-		//doGet(request, response);
+//		System.out.println(result);
+//		//doGet(request, response);
+		
+		if(result.equals("Valid")) {
+			
+		}
+		else {
+			session.setAttribute("msg", result);
+			response.sendRedirect("contact.jsp");
+		}
 	}
 
 }
---
+++ Restro/src/main/webapp/message.jsp
@@ -0,0 +1,12 @@
+<%
+
+String message=(String)session.getAttribute("msg");
+
+
+if(message!=null)
+{
+%>
+<%=message%>
+<% }  
+session.removeAttribute("msg");
+%>
